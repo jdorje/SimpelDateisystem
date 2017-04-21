e2aa18c8de9ca0951f534faa54c94ca933aa7e86
James Carroll <jd.carroll@rutgers.edu>
2017-04-21 16:04:29 -0400

    For now un/mount, later tests

---
+++ montiere.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+ARGS="$*"
+PATH_TO_MONTIERE="`dirname $0`"
+NAME_OF_MONTIERE="`basename $0`"
+MONTIERE_SH="$PATH_TO_MONTIERE/$NAME_OF_MONTIERE"
+
+usage() {
+	echo "Usage: $MONTIERE_SH [-m|--mount] [-u|--unmount] [-c|--check] "
+	return 0
+}
+
+mount() {
+	SFSBIN="$PATH_TO_MONTIERE/src/sfs"
+	TOMOUNT="$PATH_TO_MONTIERE/testfsfile"
+
+	${SFSBIN} ${TOMOUNT} example/mountdir
+}
+
+unmount() {
+	fusermount -u example/mountdir
+	echo "unmounted"
+}
+
+check() {
+	mount | grep sfs
+}
+
+option="${1}"
+case ${option} in
+	-[mM]) mount ;;
+	--mount) mount ;;
+	-[uU]) unmount ;;
+	--unmount) unmount ;;
+	-[cC]) check ;;
+	--count) check ;;
+	*) usage ;;
+esac
